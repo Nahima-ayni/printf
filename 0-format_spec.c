5fac531aa2f99bf52950aaef37d5f9b333494067
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-14 10:03:57 -0700

    produces output according to a format

--- 0-format_spec.c
+++ 0-format_spec.c
@@ -1,68 +1,72 @@
 #include "main.h"
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 
 /**
  * _printf - function name
  * @format: constant char
  * Return: number of characters printed
- *
  */
 int _printf(const char *format, ...)
 {
-	char *str;
 	int length = 0;
 	va_list format_spec;
 
 	va_start(format_spec, format);
-
 	while (*format)
 	{
 		if (*format == '%')
 		{
 			format++;
-		if (*format == '\0')
-			break;
-		switch (*format)
+			if (*format == '\0')
+				break;
+			switch (*format)
+			{
+				case 'c':
+					putchar(va_arg(format_spec, int));
+					length++;
+					break;
+				case 's':
+					_string(va_arg(format_spec, char *));
+					break;
+				case '%':
+					putchar('%');
+					length++;
+					break;
+				default:
+					putchar('%');
+					length++;
+					break;
+			}
+		}
+		else
 		{
-			case 'c':
-				putchar(va_arg(format_spec, int));
-				length++;
-				break;
-			case 's':
-				str = va_arg(format_spec, char *);
-				if (str != NULL)
-				{
-					while (*str)
-					{
-						putchar(*str);
-						length++;
-						str++;
-					}
-				}
-				break;
-			case '%':
-				putchar('%');
-				length++;
-				break;
-			default:
-				putchar('%');
-				length++;
-				break;
+			putchar(*format);
+			length++;
 		}
-		}
-	else
-	{
-		putchar(*format);
-		length++;
-	}
-	format++;
+		format++;
 	}
 	va_end(format_spec);
 	return (length);
-
 }
 
+/**
+ * _string - functin to print to string
+ * @str: string
+ * Return: always succes
+ */
+int _string(char *str)
+{
+	int length = 0;
 
+	if (str != NULL)
+	{
+		while (*str)
+		{
+			putchar(*str);
+			length++;
+			str++;
+		}
+	}
+	return (length);
+}
