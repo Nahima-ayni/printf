ecbf26c9e482097c1afb73abcaafa5076148da2a
Nahima-ayni <nahimamadey1@gmail.com>
2023-10-14 22:43:43 -0700

    produces output according to a format

--- test/0-format_spec.c
+++ test/0-format_spec.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdarg.h>
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * _printf - function name
  * @format: constant char
@@ -9,10 +9,8 @@
  */
 int _printf(const char *format, ...)
 {
-	int length = 0;
+	int length = 0, indexbuffer = 0;
 	char _buffer[SIZE_OF_BUFFER];
-	int indexbuffer = 0;
-	int str_len = 0;
 	va_list format_spec;
 
 	va_start(format_spec, format);
@@ -23,25 +21,14 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == '\0')
 				break;
-			switch (*format)
-			{
-				case 'c':
-					_buffer[indexbuffer++] = (va_arg(format_spec, int));
-					length++;
-					break;
-				case 's':
-					str_len = _string(va_arg(format_spec, char *), _buffer, &indexbuffer);
-					length += str_len;
-					break;
-				case '%':
-					_buffer[indexbuffer++] = ('%');
-					length++;
-					break;
-				default:
-					_buffer[indexbuffer++] = ('%');
-					length++;
-					break;
-			}
+			if (*format == 'c')
+				_buffer[indexbuffer++] = va_arg(format_spec, int);
+			else if (*format == 's')
+				length += _string(va_arg(format_spec, char *), _buffer, &indexbuffer);
+			else if (*format == '%')
+				_buffer[indexbuffer++] = '%';
+			else
+				_buffer[indexbuffer++] = '%';
 		}
 		else
 		{
@@ -60,11 +47,14 @@ int _printf(const char *format, ...)
 /**
  * _string - functin to print to string
  * @str: string
+ * @_buffer: arrays
+ * @indexbuffer: index of arrays
  * Return: always succes
  */
 int _string(char *str, char _buffer[], int *indexbuffer)
 {
 	int length = 0;
+
 	if (str != NULL)
 	{
 		while (*str)
@@ -75,7 +65,7 @@ int _string(char *str, char _buffer[], int *indexbuffer)
 			str++;
 
 			if (*indexbuffer == SIZE_OF_BUFFER)
-				printbuff(_buffer, &indexbuffer);
+				printbuff(_buffer, indexbuffer);
 		}
 	}
 	return (length);
