b84756ac06e3fb01e008921d156698b16769d2ac
codeniaaz <muktarabdiaziz12@gmail.com>
2023-10-17 12:35:37 -0700

    created a binary specifier

---
+++ 2-aynii_binary.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ */
+int b_specifier(va_list format_specifier)
+{
+	unsigned int nums = va_arg(format_specifier, unsigned int);
+	unsigned int binary[32];
+	int i = 0, length = 0;
+	char digit_char;
+
+	if (nums == 0)
+	{
+		write(1, "0", 1);
+		return (1);
+	}
+
+	while (nums > 0)
+	{
+		binary[i] = nums % 2;
+		nums = nums / 2;
+		i++;
+	}
+
+	length = i;
+	for (i = length - 1; i >= 0; i--)
+	{
+		digit_char = binary[i] + '0';
+		write(1, &digit_char, 1);
+	}
+
+	return (length);
+}
